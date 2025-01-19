8f232d074f31cf93fb9175cb7afba825944b2cbd
Mohammad Aakil <aakilk735@gmail.com>
2025-01-19 17:14:45 +0530

    Add files via upload

---
+++ String_Concatenate.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class String_Concatenate {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Entet the first String= ");
+        String str1=sc.nextLine();
+        System.out.print("Enter the second string= ");
+        String str2=sc.nextLine();
+        System.out.print(str1+str2+"\t");
+    }
+}
