83b5ee07a5794e7774ac764af9c73dccddbe942d
Mohammad Aakil <aakilk735@gmail.com>
2025-01-19 17:08:36 +0530

    Add files via upload

---
+++ Print_String.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Print_String {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String= ");
+        String str=sc.nextLine();
+        System.out.print(str+"\t");
+    }
+}
