a1a04a6d885c7a18b3f7a3dba6633ad8864c99fd
Mohammad Aakil <aakilk735@gmail.com>
2025-01-23 21:24:46 +0530

    Add files via upload

---
+++ Reverse_String.java
@@ -0,0 +1,16 @@
+
+class Reverse_String {
+    public static void main(String[] args) {
+
+        String s = "Aakil";
+        String r = "";
+        char ch;
+
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            r = ch + r;
+        }
+
+        System.out.println(r);
+    }
+}
